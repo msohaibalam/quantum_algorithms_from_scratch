d96368cfe02f1fd9ff7c6bd8163a9c951c8d018b
M. Sohaib Alam <sohaib.alam@gmail.com>
2018-04-02 11:44:47 -0700

    Completing basis with vector not orthogonal to period vector

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def row_echelon_form(L):
     '''
     Input: The augmented matrix of
@@ -6,9 +9,26 @@ def row_echelon_form(L):
     Output: Row echelon form of the matrix L
     '''
     num_rows = len(L)
+    # return None if empty list/array
+    if num_rows == 0:
+        return None
     num_cols = len(L[0])
-    # Loop through columns
-    for i in range(num_cols):
+    # Loop through columns, handling for non-square matrices as well
+    for i in range(min(num_cols, num_rows)):
+        # search for max in this column
+        max_el = abs(L[i][i])
+        max_row = i
+        for k in range(i + 1, num_rows):
+            if abs(L[k][i]) > max_el:
+                max_el = abs(L[k][i])
+                max_row = k
+
+        # swap max row with current row
+        for k in range(i, num_cols):
+            tmp = L[max_row][k]
+            L[max_row][k] = L[i][k]
+            L[i][k] = tmp
+
         # making all cells below current cell 0
         for k in range(i + 1, num_rows):
             # calculate the multiplier
@@ -26,15 +46,41 @@ def row_echelon_form(L):
 
 def solve_row_echelon_form(R):
     '''
-    Input: A list of lists R, providing the upper rectangular form
+    Input: A list of lists R, providing the row echelon form
         of augmented matrix obtained via allowed row-operations
     Output: Solution of the linear equations
+        (provided as a list, or numpy array)
     '''
     num_rows = len(R)
+    # return None if empty list/array
+    if num_rows == 0:
+        return None
+    # check if not a square (non-augmented) matrix
     num_cols = len(R[0])
+    if num_rows < num_cols - 1:
+
+        ## There are 2 scenarios here:
+        ## (a) One can complete the basis with a vector not orthogonal to s
+        ##  and solve (see section 18.13.2 in Quantum Computing for the Community College)
+        ## (b) We genuinely don't have enough equations to solve all variables
+
+        # loop through diagonal to check for situation (a)
+        for k in range(num_rows):
+            if R[k][k] == 0:
+                if type(R) == list:
+                    R = R[:k] + [[1 if ((kk == k) or (kk == num_cols - 1)) else 0 for kk in range(num_cols)]] + R[k:]
+                elif type(R).__module__ == np.__name__:
+                    R = np.vstack((np.vstack((R[:k], np.array([1 if ((kk == k) or (kk == num_cols - 1)) else 0 for kk in range(num_cols)], dtype=float))), R[k:]))
+
+    # recompute num_rows and num_cols
+    num_rows = len(R)
+    num_cols = len(R[0])
+    # return list of Nones if not enough eqns for vars
     if num_rows < num_cols - 1:
         print ("Not enough equations to solve all variables")
         return [None for _ in range(num_rows)]
+
+    # solve if enough eqns for vars
     x = [0 for _ in range(num_rows)]
     ## starting with bottom-most row, solve for all the variables
     ## by back-substitution
@@ -49,6 +95,7 @@ def solve_row_echelon_form(R):
         x[i] = R[i][num_cols - 1]/R[i][q]
         for j in range(i - 1, -1, -1):
             R[j][num_cols - 1] -= R[j][i] * x[i]
+
     return x
 
 
@@ -68,4 +115,7 @@ def rank(A):
     Computes the rank of matrix A
     '''
     R = row_echelon_form(A)
+    # return 0 for empty list input
+    if R is None:
+        return 0
     return len([l for l in R if set(l) != set([0])])
--- test_gaussian_elimination.py
+++ test_gaussian_elimination.py
@@ -8,8 +8,8 @@ class TestGaussianElimination(unittest.TestCase):
     def test_row_echelon_form_3vars(self):
         A = [[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3]]
         result = row_echelon_form(A)
-        compare = [[2, 1, -1, 8], [0, 0.5, 0.5, 1.0], [0, 0, -1.0, 1.0]]
-        self.assertEqual(result, compare)
+        compare = np.array([[-3, -1, 2, -11], [0, 1.6666666666666665, 0.6666666666666667, 4.333333333333333], [0, 0, 0.19999999999999987, -0.19999999999999984]])
+        self.assertTrue(np.allclose(result, compare))
 
     def test_solve_row_echelon_form_3vars(self):
         R = [[2, 1, -1, 8], [0, 0.5, 0.5, 1.0], [0, 0, -1.0, 1.0]]
@@ -20,8 +20,8 @@ class TestGaussianElimination(unittest.TestCase):
     def test_gauss_eliminate_3vars(self):
         A = [[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3]]
         result = gauss_eliminate(A)
-        compare = [2, 3, -1]
-        self.assertEqual(result, compare)
+        compare = np.array([2, 3, -1])
+        self.assertTrue(np.allclose(result, compare))
 
     def test_row_echelon_form_4vars(self):
         A = [[1, 0, 1, -1, 0], [1, -1, 0, 1, 0], [1, 1, -1, 0, 0],
@@ -48,15 +48,15 @@ class TestGaussianElimination(unittest.TestCase):
     def test_gauss_eliminate_3vars_4eqns(self):
         A = [[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3], [-4, 2, 4, -6]]
         result = gauss_eliminate(A)
-        compare = [2, 3, -1]
-        self.assertEqual(result, compare)
+        compare = np.array([2, 3, -1])
+        self.assertTrue(result, compare)
 
     def test_gauss_eliminate_3vars_5eqns(self):
         A = [[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3], [-4, 2, 4, -6],
             [6, 2, -4, 22]]
         result = gauss_eliminate(A)
-        compare = [2, 3, -1]
-        self.assertEqual(result, compare)
+        compare = np.array([2, 3, -1])
+        self.assertTrue(np.allclose(result, compare))
 
     def test_rank_3vars(self):
         A = [[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3]]
@@ -81,7 +81,7 @@ class TestGaussianElimination(unittest.TestCase):
     def test_row_echelon_form_3vars_array(self):
         A = np.array([[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3]], dtype=float)
         result = row_echelon_form(A)
-        compare = np.array([[2, 1, -1, 8], [0, 0.5, 0.5, 1.0], [0, 0, -1.0, 1.0]])
+        compare = np.array([[-3, -1, 2, -11], [0, 1.6666666666666665, 0.6666666666666667, 4.333333333333333], [0, 0, 0.19999999999999987, -0.19999999999999984]])
         self.assertTrue(np.all(result == compare))
 
     def test_row_echelon_form_4vars_array(self):
@@ -104,12 +104,30 @@ class TestGaussianElimination(unittest.TestCase):
                       [-2, 1, 2, -3], [-4, 2, 4, -6]], dtype=float)
         result = gauss_eliminate(A)
         compare = np.array([2, 3, -1])
-        self.assertTrue(np.all(result == compare))
+        self.assertTrue(np.allclose(result, compare))
 
     def test_rank_not_enough_eqns(self):
         A = np.array([[2, 1, -1, 8], [-3, -1, 2, -11]], dtype=float)
         result = rank(A)
         compare = 2
+        self.assertEqual(result, compare)
+
+    def test_rank_empty_list(self):
+        A = []
+        result = rank(A)
+        compare = 0
+        self.assertEqual(result, compare)
+
+    def test_complete_basis(self):
+        A = [[0, 0, 1, 0], [1, 0, 0, 0]]
+        result = gauss_eliminate(A)
+        compare = [0.0, 1.0, 0.0]
+        self.assertEqual(result, compare)
+
+    def test_complete_basis_array(self):
+        A = np.array([[0, 0, 1, 0], [1, 0, 0, 0]], dtype=float)
+        result = gauss_eliminate(A)
+        compare = [0.0, 1.0, 0.0]
         self.assertEqual(result, compare)
 
 if __name__ == "__main__":
